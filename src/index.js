56fbdcad763ccd5c1a20fc8f45da2ea0a0ad6085
李丙阳 <1731874942@qq.com>
2021-02-25 19:45:31 +0800

    learn: 补充 单例模式

--- src/index.js
+++ src/index.js
@@ -29,10 +29,35 @@ p.init()
 
 
 /**
- * 单例模式： 系统中唯一被使用，一个类只有一个实例
+ * 单例模式：一个对象只允许创建一个实例，那么这个类就是单例类，这种设计模式叫做单例设计模式，简称为单例模式
  * 经典应用场景：登录框、购物车、JQuery的$、vuex、redux
+ * 优点：
+ * 划分独立的命名空间，避免全局变量污染
+ * 代码逻辑集中，业务高低内聚
+ * 只创建一个实例，减少对象频繁创建、销毁的消耗
+ * 缺点：
+ * 对oop特性的支持不好，单例模式内部自己创建实例，外部无法通过new来实例化，无法继承扩展
+ * 对代码可测试性不友好
  */
 
+ // 使用静态方法实现
+class singleLoading {
+    show () {
+        //...
+    }
+    static getInstance() {
+        if(!singleLoading.instance) {
+            singleLoading.instance = new singleLoading()
+        }
+        return singleLoading.instance
+    }
+}
+
+const loading1 = singleLoading.getInstance()
+const loading2 = singleLoading.getInstance()
+console.log('单例只会创建一次', loading1 === loading2)
+
+ // 使用闭包实现
 class SingleObject {
     login() {
         console.log('单例模式')
